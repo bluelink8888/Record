1ed6df0313df528887e900ede0bce74c415b8eb7
bluelink8888 <bluelink8888@gmail.com>
2021-01-21 12:00:02 +0800

    Add controller and service test

--- restapi/src/test/java/com/github/bluelink/restapi/RestapiApplicationTests.java
+++ restapi/src/test/java/com/github/bluelink/restapi/RestapiApplicationTests.java
@@ -1,7 +1,9 @@
 package com.github.bluelink.restapi;
 
 import com.github.bluelink.restapi.config.TestMysqlDatasource;
+import com.github.bluelink.restapi.controller.SystemController;
 import com.github.bluelink.restapi.repository.SystemRepository;
+import com.github.bluelink.restapi.service.SystemService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,27 +13,51 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
+/**
+ * Spring boot basic test and datasource test
+ */
 @SpringBootTest
 @Import(TestMysqlDatasource.class)
-/**
- * Spring boot default test and datasource test
- */
 class RestapiApplicationTests {
 
   @Autowired
   private SystemRepository systemRepository;
 
+  @Autowired
+  private SystemService systemService;
+
+  @Autowired
+  private SystemController systemController;
+
   @Test
   void contextLoads() {
   }
 
+  /**
+   * Here start basic test case in this application
+   */
+
+  // This test make sure mybatis initial instance success
   @Test
   void testRepositoryIsExist() {
     assertThat(systemRepository).isNotNull();
   }
 
+  // This test make sure repository will return expect result, Optional String test is expect value.
   @Test
   void testDatasoruceIsConnected() {
     assertThat(systemRepository.getStringFromDatabase()).isEqualTo(Optional.of("test"));
+  }
+
+  // This test make sure service layer will return same result like repository.
+  @Test
+  void testSystemServiceGetStringFromDatabaseIsEqualToTest() {
+    assertThat(systemService.getStringFromDatabase()).isEqualTo(Optional.of("test"));
+  }
+
+  // This test make sure controller return value contain expect value.
+  @Test
+  void testSystemControllerGetStringFromDatabase() {
+    assertThat(systemController.getStringFromDatabase()).containsValues("test");
   }
 }
