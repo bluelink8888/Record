a6442c919ac360cac89c1f54d30c045cab026792
bluelink8888 <bluelink8888@gmail.com>
2021-02-03 17:47:10 +0800

    combine compress string and decompress string test, and add archive object test case

--- restapi/src/test/java/com/github/bluelink/restapi/utils/TestLZ4ArchiveUtils.java
+++ restapi/src/test/java/com/github/bluelink/restapi/utils/TestLZ4ArchiveUtils.java
@@ -1,5 +1,7 @@
 package com.github.bluelink.restapi.utils;
 
+import com.github.bluelink.restapi.model.serialize.SerializableChild;
+import com.github.bluelink.restapi.model.serialize.SerializeObjectMather;
 import org.junit.jupiter.api.Test;
 
 import java.nio.charset.StandardCharsets;
@@ -10,23 +12,34 @@ public class TestLZ4ArchiveUtils {
 
   private final String testString = "test";
 
-  @Test
-  public void testCompressWithoutError(){
-    try {
-      assertThat(LZ4ArchiveUtils.compress(testString.getBytes(StandardCharsets.UTF_8))).isNotNull();
+  SerializableChild serializableChild = SerializeObjectMather.getSerializableChild();
 
+  @Test
+  public void testCompressAndDecompressStringWithoutError() {
+    try {
+      // Compress no error
+      String compressResult = LZ4ArchiveUtils.compress(testString.getBytes(StandardCharsets.UTF_8));
+      assertThat(compressResult).isNotNull();
+      // Decompress will equal to origin testString
+      assertThat(new String(LZ4ArchiveUtils.decompress(compressResult), StandardCharsets.UTF_8)).isEqualTo(testString);
     } catch (Exception e) {
       System.err.println(e.getMessage());
     }
   }
 
   @Test
-  public void testSafeDecompressWithoutError(){
+  public void testSerializeAndArchiveWithoutError() {
     try {
-      System.out.println(new String(LZ4ArchiveUtils.decompress(LZ4ArchiveUtils.compress(testString.getBytes(StandardCharsets.UTF_8))), StandardCharsets.UTF_8));
+      String serializeResult = SerializeUtils.serializeObject(serializableChild);
+      assertThat(serializeResult).isNotNull();
+      String compressResult =  LZ4ArchiveUtils.compress(serializeResult.getBytes(StandardCharsets.UTF_8));
+      assertThat(compressResult).isNotNull();
+      String decompressResult = new String(LZ4ArchiveUtils.decompress(compressResult), StandardCharsets.UTF_8);
+      assertThat(decompressResult).isNotNull();
+      assertThat(SerializeUtils.deserialize(decompressResult)).isEqualTo(serializableChild);
     } catch (Exception e) {
       System.err.println(e.getMessage());
     }
-
   }
+
 }
