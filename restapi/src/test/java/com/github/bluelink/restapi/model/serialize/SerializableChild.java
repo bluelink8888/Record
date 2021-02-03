d4d348b2e1568b36118138c90307c0facdcd832d
bluelink8888 <bluelink8888@gmail.com>
2021-02-03 17:44:45 +0800

    Add equal and hashcode method in test class

--- restapi/src/test/java/com/github/bluelink/restapi/model/serialize/SerializableChild.java
+++ restapi/src/test/java/com/github/bluelink/restapi/model/serialize/SerializableChild.java
@@ -1,12 +1,13 @@
 package com.github.bluelink.restapi.model.serialize;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
 
-@Getter @Setter @NoArgsConstructor
+@Setter @Getter @NoArgsConstructor @EqualsAndHashCode
 public class SerializableChild extends SerializableParent implements Serializable {
 
   private String description;
